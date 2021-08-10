61b3900056b5a626b1c59ab336ccccb066e6c8fe
Attila Király <kiralyattila.hu@gmail.com>
2021-08-10 21:51:41 +0200

    Finish implementing game

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,4 +1,8 @@
 import androidx.compose.runtime.*
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.css.Color.black
+import org.jetbrains.compose.web.css.Color.lightgray
+import org.jetbrains.compose.web.css.LineStyle.Companion.Solid
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 
@@ -37,26 +41,70 @@ data class TicTacToeState(val state: List<MutableList<Int>>) {
         state[rowIndex][colIndex] = next
         next *= -1
     }
+
+    val finished: Boolean
+        get() = winner != null
+
+    val winner: Int?
+        get() {
+            val count = state.size
+            return ((0 until count).asSequence().map { c -> state.asSequence().map { it[c] } }
+                    + (0 until count).asSequence().map { c -> state[c].asSequence() } +
+                    generateSequence {
+                        state.asSequence().mapIndexed { index, list -> list[index] }
+                    }.take(1) +
+                    generateSequence {
+                        state.asSequence().mapIndexed { index, list -> list[count - 1 - index] }
+                    }.take(1))
+                .map { it.toSet() }
+                .find { it.size == 1 && !it.contains(0) }
+                ?.first()
+        }
 }
 
 @Composable
 fun TicTacToe(state: TicTacToeState, onChange: (TicTacToeState) -> Unit) {
-    println(state)
-    Table {
+    val winner = state.winner
+
+    Table({
+        style {
+            border(1.px, Solid, black)
+        }
+    }
+    ) {
         state.state.forEachIndexed { rowIndex, row ->
             Tr {
                 row.forEachIndexed { colIndex, value ->
-                    Td(attrs = {
-                        onClick { event ->
-                            state.click(rowIndex, colIndex)
-                            onChange(state)
+                    Td({
+                        style {
+                            width(100.px)
+                            height(100.px)
+                            padding(0.px)
+                            margin(0.px)
+                            //border(1.px, Solid, black)
+                            textAlign("center")
+                            attr("vertical-align", "center")
+                            if (rowIndex % 2 != colIndex % 2) backgroundColor(lightgray)
+                        }
+                        if (winner == null && value == 0) {
+                            onClick {
+                                state.click(rowIndex, colIndex)
+                                onChange(state)
+                            }
                         }
                     }) {
-                        if (value < 0) Text("X")
-                        else if (value > 0) Text("O")
+                        when {
+                            value < 0 -> Text("X")
+                            value > 0 -> Text("O")
+                        }
                     }
                 }
             }
         }
     }
+
+    if (winner != null)
+        Div({ style { fontWeight("bold") } }) {
+            Text("${if (winner < 0) "X" else "O"} won!")
+        }
 }
